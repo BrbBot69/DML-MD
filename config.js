8e5926bada0c2423e3dc7c419bae09dc998d8509
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-08-08 23:27:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY DML-TECH 🔰*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY Ballas 𝗑ᴾᴿᴼ*",
 // set the auto reply massage on status reply  
 WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
@@ -27,9 +27,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/z2rr5a.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "DML-TECH",
+BOT_NAME: process.env.BOT_NAME || "Ballas 𝗑ᴾᴿᴼ",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "DML-TECH",
+STICKER_NAME: process.env.STICKER_NAME || "Ballas 𝗑ᴾᴿᴼ",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,17 +37,17 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255615752312",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "27767494368",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DML",
+OWNER_NAME: process.env.OWNER_NAME || "♤𝗑ᴾᴿᴼ♧",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© Powered by DML*",
+DESCRIPTION: process.env.DESCRIPTION || "*© Powered by ♤𝗑ᴾᴿᴼ♧*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/z2rr5a.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *DML-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *Ballas 𝗑ᴾᴿᴼ*⚡",
 // add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
+READ_MESSAGE: process.env.READ_MESSAGE || "true",
 // Turn true or false for automatic read msgs
 AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
@@ -63,7 +63,7 @@ AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "255615752312",
+DEV: process.env.DEV || "27767494368",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
